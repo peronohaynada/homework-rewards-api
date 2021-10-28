636aebc099f05a9fd18be5f492b64a607c03d6cf
Mathias Barreneche <image.mathias@gmail.com>
2021-10-28 01:39:44 -0300

    Added a custom response to the built in Spring boot Error Controller

---
+++ src/main/java/com/homework/rewards/error/RewardsCustomErrorAttributes.java
@@ -0,0 +1,21 @@
+package com.homework.rewards.error;
+
+import java.util.Map;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+@Component
+public class RewardsCustomErrorAttributes extends DefaultErrorAttributes {
+	@Override
+    public Map<String, Object> getErrorAttributes(
+      WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = 
+          super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("help!", "If error persists please raise a ticket at bugs.sos");
+
+        return errorAttributes;
+    }
+}
